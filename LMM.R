ca55cdcfef3574f3c1c990d4e0e2ccc6b4d623ba
hectormacg <hecma@LAPTOP-ON8QPUK9>
2024-11-06 15:42:29 +0000

    last changes

--- LMM.R
+++ LMM.R
@@ -14,4 +14,15 @@ LMMsetup <- function(form, dat, ref = list()) {
   
   return(list(X = X, Z=Z,y=y))
 }
-LMMsetup(score ~ Machine,Machines,list("Worker",c("Worker","Machine")))
+dim(result$Z)
+qrZ<-qr(result$Z)
+dim(qr.R(qrZ))
+LMMprof<-function(X, Z, y){
+          qrZ<-qr(Z)
+  
+}
+result<-LMMsetup(score ~ Machine,Machines,list("Worker",c("Worker","Machine")))
+dim(resul$Z)
+unique(Machines$Worker)
+unique(Machines$Machine)
+colnames(resul$Z)
